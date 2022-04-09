6591a30dd0781b533c75330ee8e162018b64e07d
G-Sackey <o.m.a.r.t.o.g.a.s@gmail.com>
2022-04-08 18:58:49 -0700

    Print alphas

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
+/**
  *main - Entry point
  *
  *Return: Always 0 (Success)
- */
+ **/
 int main(void)
 {
 char ch;
@@ -12,6 +12,5 @@ for (ch='a'; ch<='z'; ch++)
 putchar(ch);
 }
 putchar('\n');
-
 return (0);
 }
