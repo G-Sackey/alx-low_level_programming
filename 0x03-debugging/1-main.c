98cede3022028f6d4a7e4269fbced73911404998
G-Sackey <o.m.a.r.t.o.g.a.s@gmail.com>
2022-04-15 16:26:34 -0700

    Infinte loop wahala

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - infinte loop
+*
+*Return: 0 
+*/
+int main(void)
+{
+int i;
+printf("infinite loop incoming :(\n");
+i = 0;
+/*
+*while (i < 10)
+*{
+*Infinite loop - No increase of varaible
+*putchar(i);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
