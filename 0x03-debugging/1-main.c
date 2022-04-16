bdc4c42e91ca2a3e65f6c2951552969f3fbd7bc6
G-Sackey <o.m.a.r.t.o.g.a.s@gmail.com>
2022-04-15 17:27:10 -0700

    Main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
-*main - infinte loop
-*
-*Return: Always 0
+*main - causes an infinite loop
+*Return: 0
 */
 int main(void)
 {
@@ -12,8 +11,7 @@ i = 0;
 /*
 *while (i < 10)
 *{
-*Infinite loop - No increase of varaible
-*putchar(i);
+*		putchar(i);
 *}
 */
 printf("Infinite loop avoided! \\o/\n");
